d25cc3ab0bdac3cc9cb9f267191d4ee465760d10
mohamedbaker <dam.vampire22@gmail.com>
2024-01-14 12:25:19 +0200

    update method in console setattr issue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,11 +18,26 @@ class FileStorage:
     def new(self, obj):
         """ Adds 'obj' to the dictionary containing all objects """
         key = ".".join([obj.__class__.__name__, obj.id])
+        '''
+        got error that can't pass dictionery to obj .to dict tried below code 
+           And tried to convert the dict to obj same error : will get back to it .
+        if  isinstance(obj, dict):
+             FileStorage.__objects[key] = obj
+             self.save()
+        else: 
+        '''
         FileStorage.__objects[key] = obj.to_dict()
         self.save()
 
     def save(self):
         """ Serializes the main object dictionary to a JSON file """
+        all_objs = FileStorage.__objects
+
+        obj_dict = {}
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
         with open(FileStorage.__file_path, "w") as f:
             json.dump(FileStorage.__objects, f)
 
